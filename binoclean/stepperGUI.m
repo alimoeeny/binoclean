32c5328a42cf37697d4a7169e98e5a4f7a2a8b87
Ali Moeeny <alimoeeny@gmail.com>
2012-03-01 11:23:56 -0500

    Stepper is opened every time the stepper window is opened

--- binoclean/stepperGUI.m
+++ binoclean/stepperGUI.m
@@ -27,13 +27,14 @@ extern char *stepport;
 - (void)windowDidLoad
 {
     [super windowDidLoad];
-    
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    NSLog(@"opening Stepper");
+    OpenStepSerial(stepport);
 }
 
 
 - (IBAction) uDriveInitialize
 {
+    NSLog(@"opening Stepper");
     OpenStepSerial(stepport);
 }
 
