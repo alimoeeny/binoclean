5a7e57589df5145d317e83668cce456825279355
Ali Moeeny <alimoeeny@gmail.com>
2012-03-15 15:27:54 -0400

    To prevent screensaver and sleep!

--- binoclean/main.m
+++ binoclean/main.m
@@ -17,13 +17,32 @@
 int binocmain(int argc, char * argv[]);
 char * VERSION_NUMBER;
 
+
+// to prevent screen saver and system going to sleep
+void caffeineTimerCallback(CFRunLoopTimerRef timer, void *info)
+{
+    UpdateSystemActivity(OverallAct);
+}
+
 int main(int argc, char *argv[])
 {
     setpriority(PRIO_PROCESS, 0, -10);
     
     NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
     VERSION_NUMBER = [version UTF8String];    
+
+    CFRunLoopTimerContext context = { 0, NULL, NULL, NULL, NULL };    
+    CFRunLoopTimerRef caffeineTimer = CFRunLoopTimerCreate(NULL, CFAbsoluteTimeGetCurrent(), 30, 0, 0, caffeineTimerCallback, &context);
+    if (caffeineTimer != NULL); {
+        CFRunLoopAddTimer(CFRunLoopGetCurrent(), caffeineTimer, kCFRunLoopCommonModes);
+    }
+    
     binocmain(argc, argv);
+    
     return NSApplicationMain(argc, (const char **)argv);
+    
     [version release];
+    
+    CFRunLoopTimerInvalidate(caffeineTimer);
+    CFRelease(caffeineTimer);
 }
