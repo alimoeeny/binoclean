a8109f177a19a59e5c35366062e11de0a2ca87a1
Ali Moeeny <alimoeeny@gmail.com>
2012-03-29 14:29:06 -0400

    labjack should be good to go!

--- binoclean/labjack_dio.c
+++ binoclean/labjack_dio.c
@@ -11,7 +11,7 @@
 
 static int isok = 0;
 
-HANDLE hDevice = 0;
+static HANDLE hDevice = 0;
 
 int DIOInit()
 {
@@ -20,6 +20,7 @@ int DIOInit()
     {
         return -1;
     }else {
+        isok = 1;
         return 0;
     }
 }
@@ -30,7 +31,41 @@ int DIOWrite(int output)
     if(!isok)
         return(0);
     
-    if (eDO(hDevice, output, 1)!=0)
+    int r = 0;
+    
+//    for (int i = 0; i < 20; i++) {
+//        if (output & i) 
+//            r = r + eDO(hDevice, i, 1); 
+//        else 
+//            r = r + eDO(hDevice, i, 0);
+//    }
+
+    switch (output) {
+        case 0:
+            r = r + eDO(hDevice, 0, 0);
+            r = r + eDO(hDevice, 1, 0);
+            r = r + eDO(hDevice, 2, 0);
+            r = r + eDO(hDevice, 3, 0);
+            break;
+        case 2:
+            r = r + eDO(hDevice, 2, 1);
+            break;
+        case 4:
+            r = r + eDO(hDevice, 3, 1);
+            break;
+        case 7:
+        case 0xF:
+            r = r + eDO(hDevice, 0, 1);
+            r = r + eDO(hDevice, 1, 1);
+            r = r + eDO(hDevice, 2, 1);
+            r = r + eDO(hDevice, 3, 1);
+            break;
+
+        default:
+            break;
+    }
+    
+    if (r!=0)
     {
         printf("DIO ERROR while writing");
     }
